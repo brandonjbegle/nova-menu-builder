9b4cba111e8ba5feea5a77ead2b5c75660822751
Brandon Begle <brandonjbegle@gmail.com>
2022-04-13 20:59:51 -0400

    logging some views

--- resources/js/menu-builder.js
+++ resources/js/menu-builder.js
@@ -4,7 +4,9 @@ import MenuBuilder from './components/MenuBuilder';
 import MenuBuilderField from './components/MenuBuilderField';
 
 Nova.booting((app, store) => {
+  console.log('app components below');
   console.log(app.components);
+  console.log('app components above');
   app.use(ToggleButton);
   app.component('menu-builder', MenuBuilder);
   app.component('form-menu-builder-field', MenuBuilderField);
