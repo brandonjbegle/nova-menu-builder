8d0fc8101c502ce9869d4e895e435145b5ca38b2
Brandon Begle <brandonjbegle@gmail.com>
2022-04-13 21:17:56 -0400

    Updates to laravel mix

--- resources/js/menu-builder.js
+++ resources/js/menu-builder.js
@@ -4,19 +4,9 @@ import MenuBuilder from './components/MenuBuilder';
 import MenuBuilderField from './components/MenuBuilderField';
 
 Nova.booting((app, store) => {
-  console.log('app components below');
-  console.log(app);
-  console.log('app components above');
   app.use(ToggleButton);
   app.component('menu-builder', MenuBuilder);
   app.component('form-menu-builder-field', MenuBuilderField);
   app.component('detail-menu-builder-field', MenuBuilderField);
-  // Nova.inertia('MenuBuilder', MenuIndexView);
-})
-
-Nova.booted((app) => {
-  console.log('app components below');
-  console.log(app);
-  console.log('app components above');
   Nova.inertia('MenuBuilder', MenuIndexView);
 })
--- webpack.mix.js
+++ webpack.mix.js
@@ -3,13 +3,17 @@ let path = require('path')
 
 mix.alias({
   'laravel-nova': path.join(__dirname, 'vendor/laravel/nova/resources/js/mixins/packages.js'),
-  'laravel-nova-views': path.join(__dirname, 'vendor/laravel/nova/resources/js/views'),
 })
 
 mix
   .setPublicPath('dist')
   .js('resources/js/menu-builder.js', 'js')
-  .vue({
-    extractStyles: mix.inProduction() ? 'css/menu-builder.css' : false,
-    processCssUrls: false,
+  .vue({version: 3})
+  .webpackConfig({
+    externals: {
+      vue: 'Vue',
+    },
+    output: {
+      uniqueName: 'optimistdigital/nova-menu-builder',
+    }
   });
